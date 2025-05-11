303e8778af8143b6c7a6232d757fb4ca33fa216f
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-11 17:31:38 -0600

    pushing version before tailwind integration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
 // Reusable UI Component
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "../styles/ComponentStyles/NavBar.module.css";
 
 export function NavBar() {
   return (
-    <header>
-      <p>DevConnect</p>
-      <nav>
-        <ul>
-          <li>
+    <header className={styles.headerWrapper}>
+      <img src="Pando.png" alt="Pando, the DevConnect Mascot" />
+      <nav className={styles.navBar}>
+        <ul className={styles.listContainer}>
+          <li className={styles.listItem}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className={styles.listItem}>
             <Link to="feed">Feed</Link>
           </li>
-          <li>
+          <li className={styles.listItem}>
             <Link to="createpost">Create Post</Link>
           </li>
-          <li>
+          <li className={styles.listItem}>
             <Link to="profile">Profile</Link>
           </li>
         </ul>
